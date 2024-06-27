629bd9ab707b22952047f2555d95e0d32800c24b
Kleber Santos <ksantosdev@gmail.com>
2024-06-27 20:46:40 -0300

    refactor: rename variable

--- src/lot/lot.service.ts
+++ src/lot/lot.service.ts
@@ -2,13 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { Lot } from 'src/entities/lot';
 import { InjectRedis } from '@liaoliaots/nestjs-redis';
 import { Redis } from 'ioredis';
-import { FindLotDto } from 'src/dto/findLotDto';
+import { FindLotDto } from 'src/dto/input/findLotDto';
 
 @Injectable()
 export class LotService {
   private lots: Lot[] = [];
 
-  constructor(@InjectRedis() private readonly redisClient: Redis) {}
+  constructor(@InjectRedis() private readonly redis: Redis) {}
 
   createLot(lot: Lot) {
     this.lots.push(lot);
@@ -17,33 +17,27 @@ export class LotService {
 
   async findLot(findLotDto: FindLotDto) {
     if (findLotDto.name) {
-      const cachedLot = await this.redisClient.get(`lot:${findLotDto.name}`);
+      const cachedLot = await this.redis.get(`lot:${findLotDto.name}`);
       if (cachedLot) {
         return JSON.parse(cachedLot) as Lot;
       }
       const lot = this.lots.find((l) => findLotDto.name === l.name);
       if (lot) {
-        await this.redisClient.set(
-          `lot:${findLotDto.name}`,
-          JSON.stringify(lot),
-        );
+        await this.redis.set(`lot:${findLotDto.name}`, JSON.stringify(lot));
         return lot;
       }
       return undefined;
     }
 
     if (findLotDto.code) {
-      const cachedLot = await this.redisClient.get(`lot:${findLotDto.code}`);
+      const cachedLot = await this.redis.get(`lot:${findLotDto.code}`);
       if (cachedLot) {
         return JSON.parse(cachedLot) as Lot;
       }
 
       const lot = this.lots.find((l) => findLotDto.code === l.code);
       if (lot) {
-        await this.redisClient.set(
-          `lot:${findLotDto.code}`,
-          JSON.stringify(lot),
-        );
+        await this.redis.set(`lot:${findLotDto.code}`, JSON.stringify(lot));
         return lot;
       }
       return undefined;
