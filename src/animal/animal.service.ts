24864b85c4c79c87adf97a4907e9f65ae7073fb2
Kleber Santos <ksantosdev@gmail.com>
2024-06-28 01:01:24 -0300

    feat: should remove old values after creating animal

--- src/animal/animal.service.ts
+++ src/animal/animal.service.ts
@@ -2,10 +2,14 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateAnimalDto } from 'src/dto/input/createAnimalDto';
 import { lots } from 'src/database/seed';
 import { Animal } from 'src/entities/animal';
+import { InjectRedis } from '@liaoliaots/nestjs-redis';
+import { Redis } from 'ioredis';
 
 @Injectable()
 export class AnimalService {
-  createAnimal(createAnimalDto: CreateAnimalDto) {
+  constructor(@InjectRedis() private readonly redis: Redis) {}
+
+  async createAnimal(createAnimalDto: CreateAnimalDto) {
     const foundLot = lots.find((lot) => lot.code === createAnimalDto.lot);
     if (!foundLot) {
       throw new NotFoundException('Lot not found');
@@ -19,6 +23,21 @@ export class AnimalService {
     }
     const animal = new Animal(createAnimalDto.name, createAnimalDto.code);
     foundLot.addAnimal(animal);
+    await this.invalidateFindAllLotsCache();
     return animal;
+  }
+
+  private async invalidateFindAllLotsCache() {
+    const criteria = ['NAME', 'CODE'];
+    const orders = ['ASC', 'DESC'];
+
+    for (const criterion of criteria) {
+      for (const order of orders) {
+        const keys = await this.redis.keys(`lots:${criterion}:${order}`);
+        if (keys.length > 0) {
+          await this.redis.del(keys);
+        }
+      }
+    }
   }
 }
